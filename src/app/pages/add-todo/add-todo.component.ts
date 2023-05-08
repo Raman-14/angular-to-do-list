39af0dc968294cdc5b1a112cc095e8708a35aa78
Raman-14 <ramanofficial014@gmail.com>
2023-05-08 20:37:44 +0530

    changes add

--- src/app/pages/add-todo/add-todo.component.ts
+++ src/app/pages/add-todo/add-todo.component.ts
@@ -1,4 +1,10 @@
-import { Component, ContentChild, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  ContentChild,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-add-todo',
@@ -9,14 +15,7 @@ export class AddTodoComponent {
   // IGNORE CODE
   @Output() dataEvent = new EventEmitter<any>();
 
-  db: any[] = [
-    {
-      //row
-      title: 'test', //column
-      content: 'test',
-      checkbox: 'test',
-    },
-  ]; // table // local variable => make it global by storing in App Component
+  db: any[] = []; // table // local variable => make it global by storing in App Component
 
   popupVisible = 'hide-popup';
 
@@ -28,46 +27,46 @@ export class AddTodoComponent {
 
     this.db.push({
       //row
+      id: this.db.length + 1,
       title: title.value, //column
       content: content.value,
       checkbox: checkbox.value,
     });
-    console.log(this.db);
 
-    console.log(34);
-    const todoData = {
-      //row
-      title: title.value, //column
-      content: content.value,
-      checkbox: checkbox.value,
-    };
-    this.dataEvent.emit(todoData);
-    console.log(40);
+    console.log(this.db);
   }
 
-  handleupdate(title: string) {
+  handleupdate(id: string) {
     console.log('Update Logic');
-    var newtitle: any = document.getElementById("popup-title");
-    var newcontent: any = document.getElementById("popup-content");
+    var newtitle: any = document.getElementById('popup-title');
+    var newcontent: any = document.getElementById('popup-content');
+
+    console.log(newtitle.value)
+    console.log(newcontent.value)
 
     this.db.map((row: any, index) => {
-      if (title === row.title) {
+      if (id === row.id) {
         this.db[index].title = newtitle.value;
         this.db[index].content = newcontent.value;
+        this.popupVisible = 'hide-popup';
+        return;
       }
-    
     });
-   this.popupVisible = 'hide-popup';
+
     // close popup
     // popclass = hide
   }
-  handledelete(title: string) {
-    console.log('Delete Logic for ', title);
+  handledelete(id: string) {
+    console.log('Delete Logic for ', id);
 
     // loop over DB
-    this.db.map((row: any, index) => {
+    this.db.some((row: any, index) => {
       console.log(index);
-      if (title === row.title) this.db.splice(index, 1);
+      if (id === row.id) {
+        console.log('L71');
+        this.db.splice(index, 1);
+        return;
+      }
     });
   }
 
